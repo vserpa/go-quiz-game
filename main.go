a93f27a092bcbbdc3863b3d3b54158adb052a695
Mestre <fit_vmestre@nextracker.com>
2025-01-14 11:44:49 -0300

    load game data function

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type GameState struct {
@@ -35,7 +37,45 @@ func (g *GameState) Init() {
 	fmt.Printf("Welcome, %s! Let's start the quiz.\n", g.PlayerName)
 }
 
+func (g *GameState) LoadGameData() {
+	file, err := os.Open("quiz.csv")
+	if err != nil {
+		fmt.Println("Error opening quiz file:", err)
+		return
+	}
+	defer file.Close() // needs to close the file when we're done
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading CSV file:", err)
+		return
+	}
+
+	for index, record := range records {
+		if index > 0 {
+			question := Question{
+				Text:    record[0],
+				Options: record[1:5],
+				Answer:  toInt(record[5]),
+			}
+
+			g.Questions = append(g.Questions, question)
+		}
+	}
+}
+
 func main() {
 	game := GameState{}
+	go game.LoadGameData()
 	game.Init()
+}
+
+func toInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println("Error converting string to int:", err)
+		return 0
+	}
+	return i
 }
