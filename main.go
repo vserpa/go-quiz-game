9f40ccced53409c5ea5afe8b15499cffecd71ef0
Mestre <fit_vmestre@nextracker.com>
2025-01-14 11:00:05 -0300

    game init method

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type GameState struct {
 	PlayerName string
@@ -14,6 +18,24 @@ type Question struct {
 	Answer  int
 }
 
+func (g *GameState) Init() {
+	fmt.Println("Initializing game state...")
+	fmt.Println("Enter your name: ")
+	reader := bufio.NewReader(os.Stdin)
+
+	name, err := reader.ReadString('\n') // read until newline (enter)
+
+	if err != nil {
+		fmt.Println("Error reading name:", err)
+		return
+	}
+
+	g.PlayerName = name[:len(name)-1] // remove newline character
+
+	fmt.Printf("Welcome, %s! Let's start the quiz.\n", g.PlayerName)
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	game := GameState{}
+	game.Init()
 }
