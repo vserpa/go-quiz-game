39ef525a45506a55e7aa2f99941266b0066c6711
Mestre <fit_vmestre@nextracker.com>
2025-01-14 10:46:47 -0300

    game state

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type GameState struct {
+	PlayerName string
+	Score      int
+	Questions  []Question
+}
+
+type Question struct {
+	Text    string
+	Options []string
+	Answer  int
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 }
